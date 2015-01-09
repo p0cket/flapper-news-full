bd6b26f215b4864e63aa4e0801af1fa52f2e7ed0
Pocket <p0ck31@gmail.com>
2015-01-09 15:42:23 -0800

    Make small changes to profile controller

--- public/javascripts/controllers/profileController.js
+++ public/javascripts/controllers/profileController.js
@@ -10,35 +10,41 @@ function($scope, postService, post) {
     if(!scope.body) {
       return;
     }
-    postService.addBio(post._id)
-  }
-}
-]);
-
-//
-app.controller('PostCtrl', [
-'$scope',
-'postService',
-'post',
-function($scope, postService, post){
-  $scope.post = post;
-
-  $scope.addComment = function(){
-    if(!$scope.body) {
-      return;
-    }
-    postService.addComment(post._id, {
+    postService.addBio(post.username, {
       body: $scope.body,
       author: 'user',
-    }).success(function(comment) {
-      $scope.post.comments.push(comment);
+    }).success(function(XXX) {
+      $scope.post.XXX.push(XXX);
     });
     $scope.body = '';
   };
-
-  $scope.incrementUpvotes = function(comment){
-    postService.upvoteComment(post, comment);
-  };
-
 }
 ]);
+//
+// //
+// app.controller('PostCtrl', [
+// '$scope',
+// 'postService',
+// 'post',
+// function($scope, postService, post){
+//   $scope.post = post;
+//
+//   $scope.addComment = function(){
+//     if(!$scope.body) {
+//       return;
+//     }
+//     postService.addComment(post._id, {
+//       body: $scope.body,
+//       author: 'user',
+//     }).success(function(comment) {
+//       $scope.post.comments.push(comment);
+//     });
+//     $scope.body = '';
+//   };
+//
+//   $scope.incrementUpvotes = function(comment){
+//     postService.upvoteComment(post, comment);
+//   };
+//
+// }
+// ]);
